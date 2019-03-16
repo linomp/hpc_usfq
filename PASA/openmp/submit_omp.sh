2739a1e288ece25b9bc2670499a2d593c7688bb0
Usuario TESTING <test@login.hpc.cedia.org.ec>
2019-03-16 10:56:57 -0500

    first

--- PASA/openmp/submit_omp.sh
+++ PASA/openmp/submit_omp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=cuda_[tu_nombre]
+#SBATCH --job-name=cuda_linomp
 #SBATCH --output=res.txt
 #
 #SBATCH --ntasks=1
@@ -11,5 +11,17 @@
 
 srun gcc -std=c99 -fopenmp evaluar_func.c -o ahmdal.exe -lm
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=2
+srun ./ahmdal.exe
+
+export OMP_NUM_THREADS=4
+srun ./ahmdal.exe
+
+export OMP_NUM_THREADS=8
+srun ./ahmdal.exe
+
+export OMP_NUM_THREADS=16
+srun ./ahmdal.exe
+
+export OMP_NUM_THREADS=32
 srun ./ahmdal.exe
