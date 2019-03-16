c1b7b92da421b9a64252fee7f17a1dcf5e8e7fbd
linomp <lm2p@hotmail.es>
2019-03-16 09:07:13 -0500

    first commit

--- CUDA/ej_cuda_1.cu
+++ CUDA/ej_cuda_1.cu
@@ -24,7 +24,10 @@ __device__ int modulo(int a, int b){
 
 __global__ void decrypt_kernel(int *d_input, int *d_output, int length)
 {
-	// ...
+	for (int i = 0; i< length; i++){
+		char x = d_input[i];
+		d_output[i] = 111*(x-B) % M; 
+	}
 }
 
 __global__ void decrypt_multiblock_kernel(int *d_input, int *d_output, int length)
@@ -54,29 +57,29 @@ int main(int argc, char *argv[])
 	h_output = (int *)malloc(size);
 
 	// reservar memoria en la GPU para d_input y d_output
-	// ...
+	cudaMalloc(&d_input, size); cudaMalloc(&d_output, size);
 	checkCUDAError("Memory allocation");
 
 	// leo el archivo con el mensaje cifrado
 	read_file(fname, h_input);
 
 	// transferir el arreglo de entrada al dispositivo
-	// ...
+	cudaMemcpy(d_input, h_input, size, cudaMemcpyHostToDevice);
 	checkCUDAError("Input transfer to device");
 
 	// configurar la grilla de threads
-	// dim3 blocksPerGrid...
-	// dim3 threadsPerBlock...
+	dim3 blocksPerGrid (1, 1, 1) 
+	dim3 threadsPerBlock (N, 1, 1)
 
 	// ejecutar el kernel
-	// decrypt_kernel <<< ... >>>( ... );
+	decrypt_kernel <<< blocksPerGrid, threadsPerBlock >>>( d_input, d_output, length );
 
 	cudaThreadSynchronize();
 	checkCUDAError("Kernel execution");
 
 
 	// transferir el contenido de d_output a la memoria de la CPU
-	// ...
+	cudaMemcpy(h_output, d_output, size, cudaMemcpyDeviceToHost);
 	checkCUDAError("Result transfer to host");
 
 	printf("Texto desencriptado:\n");
@@ -87,7 +90,7 @@ int main(int argc, char *argv[])
 	printf("\n");
 
 	// liberar memoria en el dispositivo para d_input y d_output
-	// ...
+	cudaFree(d_input); cudaFree(d_output);
 	checkCUDAError("Free memory");
 
 	/* free host buffers */
