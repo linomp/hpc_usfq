c42671c037bc77819fd60c9443345882007a99e9
linomp <lm2p@hotmail.es>
2019-03-17 18:07:36 -0500

    fix conflict

--- CGM/LSDEMp/comm.sh
+++ CGM/LSDEMp/comm.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name="iso_compress_1000clones"
-#SBATCH --output="iso_compress_1000clones.%j"
+#SBATCH --job-name="iso_linomp" 
 #SBATCH --output=res.txt
 #SBATCH --export=ALL
 #SBATCH -p oro
 #SBATCH -w compute-0-[1]
-#SBATCH -n 24
+#SBATCH -n 16
 
 ulimit -l unlimited
 
-export OMP_NUM_THREADS=24
+export OMP_NUM_THREADS=16
 
 #mpiCC -fopenmp -I ~/eigen -o mainISO MainIso.cpp
 
-mpirun -np 1 -x OMP_NUM_THREADS=24 mainISO
+mpirun -np 1 -x OMP_NUM_THREADS=16 mainISO
